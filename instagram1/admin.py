684762de075ea55a0d6af02de4178654279cc25c
채광은(IT서비스본부/개발실/개발5팀/프로) <cls@emforce.co.kr>
2022-09-01 16:06:26 +0900

    init commit

---
+++ instagram1/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from django.utils.safestring import mark_safe
+
+from .models import Post
+
+
+# Register your models here.
+
+# admin.site.register(Post)
+
+# class PostAdmin(admin.ModelAdmin):
+#     pass
+#
+# admin.site.register(Post, PostAdmin)
+
+# Wrapping
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'photo_tag', 'massage', 'massage_length', 'is_public', 'created_at', 'updated_at']
+    list_display_links = ['massage']
+    list_filter = ['created_at', 'is_public']
+    search_fields = ['massage']
+
+    def photo_tag(self, post):
+        if post.photo:
+            return mark_safe(f"<img src='{post.photo.url}' style='width: 72px; height: 72px;'/>")
+        return None
+
+    def massage_length(self, post):
+        return f"{len(post.massage)} 글자"
+
+
