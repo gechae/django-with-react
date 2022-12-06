e9c97419ab1ad61c44be149d410e2c192d010041
채광은(IT서비스본부/개발실/개발5팀/프로) <cls@emforce.co.kr>
2022-12-06 15:43:25 +0900

    장고가 static 파일을 다루는 방법

--- askcompany/settings.py
+++ askcompany/settings.py
@@ -132,8 +132,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+#STATIC_URL = 'https://..../'
 STATIC_URL = 'static/'
-# STATIC_ROOT =
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'askcompany', 'static')
+]
 
 MEDIA_URL= '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
