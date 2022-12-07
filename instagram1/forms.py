74f145669970a5bea7946155342151b6b253b97d
채광은(IT서비스본부/개발실/개발5팀/프로) <cls@emforce.co.kr>
2022-12-07 16:45:23 +0900

    Form Validation

--- instagram1/forms.py
+++ instagram1/forms.py
@@ -1,3 +1,4 @@
+import re
 
 from django import forms
 from .models import Post
@@ -10,3 +11,9 @@ class PostForm(forms.ModelForm):
         fields = ['massage', 'photo', 'tag_set', 'is_public'] #'__all__'
         #비추천
         #exclude = ''
+
+    def clean_massage(self):
+        massage = self.cleaned_data.get('massage')
+        if massage:
+            massage = re.sub(r'[a-zA-Z]+', '', massage)
+        return massage
