3aafb1dc6692da6b385d15b1cc50dd462f31d21a
채광은(IT서비스본부/개발실/개발5팀/프로) <cls@emforce.co.kr>
2022-12-08 15:47:13 +0900

    템플릿 레이아웃에 링크 추가하기

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='accounts/login_form.html'), name="login"),
+    path('logout/', views.logout, name="logout"),
     path('profile/', views.profile, name="profile"),
     path('profile/edit', views.profile_edit, name="profile_edit"),
+    path('singup/', views.singup, name="singup"),
+
 ]
